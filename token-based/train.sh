ce1f8cad12f973c204d2b92c33fd533146f33d77
Panagiotis Georgiadis <60666912+panagiotis-geo@users.noreply.github.com>
2024-05-29 20:05:54 +0300

    Delete token-based directory

--- token-based/train.sh
+++
@@ -1,8 +0,0 @@
-poetry run python ner/train_huggingface.py\
-  --model_name_or_path bert-base-uncased \
-  --dataset_name ./original_dataset \
-  --output_dir ./test-ner \
-  --do_train \
-  --do_eval \
-  --do_predict
-  #--overwrite_output_dir
