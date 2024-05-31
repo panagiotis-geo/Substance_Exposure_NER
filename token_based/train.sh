df7965726c506cd9e0ff7b0937dc1a94525c9d41
Panagiotis Georgiadis <60666912+panagiotis-geo@users.noreply.github.com>
2024-05-31 22:35:00 +0300

    Update train.sh

--- token_based/train.sh
+++ token_based/train.sh
@@ -1,6 +1,6 @@
 poetry run python ner/train_huggingface.py\
   --model_name_or_path bert-base-uncased \
-  --dataset_name ../datasets/token_based/train-valid-test \
+  --dataset_name ../datasets/token-based/train-valid-test \
   --output_dir ./test-ner \
   --do_train \
   --do_eval \
